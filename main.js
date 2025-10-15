9adb49c5c4a6ccda81861830f4a78f1df801ed4a
Mike <mmrrff2099@gmail.com>
2025-10-15 18:59:23 +0300

    Pr3_3

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $btnKick = document.getElementById('btn-kick');
 const $btnWave = document.getElementById('btn-wave');
+const $logs = document.querySelector('#logs');
 
 let gameOver = false;
 
@@ -42,22 +43,61 @@ const enemy2 = {
     changeHP: changeHP,
 }
 
+const {name: nameCharacter } = character
+const {name: nameEnemy1 } = enemy1
+const {name: nameEnemy2 } = enemy2
+
+const battle = {
+    logs: []
+}
+
+function generateLog(firstPerson, secondPerson, damage) {
+    const logs = [
+        `${firstPerson.name.innerText} remembered something important, but suddenly ${secondPerson.name.innerText}, beside himself with fear, struck the enemy in the forearm.`,
+        `${firstPerson.name.innerText} choked, and ${secondPerson.name.innerText}, in fright, delivered a direct knee strike to the enemy's forehead.`,
+        `${firstPerson.name.innerText} forgot himself, but at that moment the impudent ${secondPerson.name.innerText}, having made a strong-willed decision, quietly approached from behind and struck.`,
+        `${firstPerson.name.innerText} came to his senses, but suddenly ${secondPerson.name.innerText} accidentally dealt a powerful blow.`,
+        `${firstPerson.name.innerText} choked, but at that moment ${secondPerson.name.innerText} reluctantly crushed his opponent with his fist \<censored\>.`,
+        `${firstPerson.name.innerText} was surprised, and ${secondPerson.name.innerText} staggered and delivered a vile blow.`,
+        `${firstPerson.name.innerText} blew his nose, but suddenly ${secondPerson.name.innerText} delivered a crushing blow.`,
+        `${firstPerson.name.innerText} staggered, and suddenly the insolent ${secondPerson.name.innerText} kicked the enemy in the leg for no reason.`,
+        `${firstPerson.name.innerText} was upset when suddenly, unexpectedly, ${secondPerson.name.innerText} accidentally kicked his opponent in the stomach.`,
+        `${firstPerson.name.innerText} tried to say something, but suddenly, unexpectedly, ${secondPerson.name.innerText}, out of boredom, broke his opponent's eyebrow.`
+    ];
+
+    const text = logs[random(logs.length) - 1]
+
+    const fullLog = `${text} -${damage} [${secondPerson.damageHP}/${secondPerson.defaultHP}]`
+
+    return fullLog;
+}
+
 function attack(character, enemy1, enemy2, maxDamage) {
+    $logs.innerHTML = '';
+
+    const newLogs = [];
+
     const damageToEnemy1 = random(maxDamage)
-    console.log(`${character.name.innerText} attack ${enemy1.name.innerText} on ${damageToEnemy1} HP`)
+    newLogs.push(generateLog(character, enemy1, damageToEnemy1))
     enemy1.changeHP(damageToEnemy1)
 
     const damageToEnemy2 = random(maxDamage)
-    console.log(`${character.name.innerText} attack ${enemy2.name.innerText} on ${damageToEnemy2} HP`)
+    newLogs.push(generateLog(character, enemy2, damageToEnemy2))
     enemy2.changeHP(damageToEnemy2)
 
     const counterDamage1 = random(maxDamage)
-    console.log(`${enemy1.name.innerText} counterattacks ${character.name.innerText} on ${counterDamage1} HP`)
+    newLogs.push(generateLog(enemy1, character, counterDamage1))
     character.changeHP(counterDamage1)
 
     const counterDamage2 = random(maxDamage)
-    console.log(`${enemy2.name.innerText} counterattacks ${character.name.innerText} on ${counterDamage1} HP`)
+    newLogs.push(generateLog(enemy2, character, counterDamage2))
     character.changeHP(counterDamage2)
+
+    newLogs.forEach(log => {
+        const $p = document.createElement('p');
+        $p.innerText = log;
+        $logs.appendChild($p);
+    });
 }
 
 function bindAttackButton(button, maxDamage, character, enemy1, enemy2) {
@@ -105,21 +145,27 @@ function changeHP(count) {
         }
     } else {
         this.damageHP -= count;
+        const log = this === enemy1 || this === enemy2 ? generateLog(this, character) : generateLog(this, enemy1) + ' ' + generateLog(this, enemy2);
+        console.log(log);
     }
     this.renderHP();
     checkGameOver();
 }
 
 function checkGameOver() {
-    if(enemy1.damageHP === 0 && enemy2.damageHP === 0 && !gameOver) {
-        alert(`${character.name.innerText} defeated both enemies!`)
+    const {name: nameCharacter, damageHP: damageHPCharacter, lost: lostCharacter } = character
+    const {damageHP: damageHPEnemy1 } = enemy1
+    const {damageHP: damageHPEnemy2 } = enemy2
+
+    if(damageHPEnemy1 === 0 && damageHPEnemy2 === 0 && !gameOver) {
+        alert(`${nameCharacter.innerText} defeated both enemies!`)
         gameOver = true;
         $btnKick.disabled = true;
         $btnWave.disabled = true;
     } 
-    if(character.damageHP === 0) {
-        alert(`${character.name.innerText} has fallen! The enemies have won.`)
-        character.lost = true;
+    if(damageHPCharacter === 0) {
+        alert(`${nameCharacter.innerText} has fallen! The enemies have won.`)
+        lostCharacter = true;
         gameOver = true;
         $btnKick.disabled = true;
         $btnWave.disabled = true;
